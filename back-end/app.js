946810a1a19469643950bf7b6cc80399cb88d399
Tana Chanchiew <42507722+terktanac@users.noreply.github.com>
2020-10-04 21:51:21 +0700

    fix bugs

--- back-end/app.js
+++ back-end/app.js
@@ -7,7 +7,7 @@ const Diary = require("./diary");
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://localhost:27017/SDS_HW", {
+mongoose.connect("mongodb://mongo:27017/SDS_HW", {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
@@ -15,13 +15,15 @@ mongoose.connect("mongodb://localhost:27017/SDS_HW", {
 app.get("/diaries", async (req, res) => {
   const diaries = await Diary.find({});
   res.json(diaries);
+  console.log("GET")
 });
 app.post("/diary", async (req, res) => {
   const payload = req.body.newDiary;
   const diary = new Diary(payload);
   await diary.save();
   res.status(201).end();
+  console.log("GET")
 });
 app.listen(9000, () => {
-  console.log("Application is running on port 9000");
+  console.log("Application is running on port 9000, new port");
 });
